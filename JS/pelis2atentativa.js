a4a0b7e175e182adb6e1666a2a720d10f0837d24
Samuel Ramírez Afonso <samuel_ra93@hotmail.com>
2017-06-14 19:15:00 +0100

    no message

---
+++ JS/pelis2atentativa.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    function buscaDatos(numeroDePagina) {
+        var urlAPI = 'https://api.themoviedb.org/3/discover/movie?api_key=e8c6d35a6bd555573d4b93aff5b6743b';
+        urlAPI += '&page=' + numeroDePagina;
+        console.log(urlAPI);
+
+
+        
+
+
+    };
+    buscaDatos(1);
+});
